da413c480ea621b8504966aa6d329396a9173e35
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-08 18:22:05 +0800

    add RasmError::Utf8Error

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,16 @@
 use std::{error::Error, fmt::Display};
 
 #[derive(Debug)]
-pub(crate) enum RasmError {
-    StateError{ cause: &'static str }
+pub enum RasmError {
+    StateError{ cause: &'static str },
+    Utf8Error{ cause: &'static str }
 }
 
 impl Display for RasmError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
             RasmError::StateError { cause } => write!(f, "{}", cause),
+            RasmError::Utf8Error { cause } => write!(f, "{}", cause),
         }
     }
 }
