82c35798e47d8a68e14b3cd16b9d83f284ececb2
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-08 16:55:47 +0800

    reorganize utility functions

---
+++ src/utils/jvm.rs
@@ -0,0 +1,30 @@
+use std::sync::{Arc, Once};
+
+use jni::{AttachGuard, InitArgsBuilder, JNIVersion, JavaVM};
+
+pub fn jvm() -> &'static Arc<JavaVM> {
+    static mut JVM: Option<Arc<JavaVM>> = None;
+    static INIT: Once = Once::new();
+
+    INIT.call_once(|| {
+        let jvm_args = InitArgsBuilder::new()
+            .version(JNIVersion::V8)
+            .option("-Xcheck:jni")
+            .build()
+            .unwrap_or_else(|e| panic!("{:#?}", e));
+
+        let jvm = JavaVM::new(jvm_args).unwrap_or_else(|e| panic!("{:#?}", e));
+
+        unsafe {
+            JVM = Some(Arc::new(jvm));
+        }
+    });
+
+    unsafe { JVM.as_ref().unwrap() }
+}
+
+pub fn attach_current_thread() -> AttachGuard<'static> {
+    jvm()
+        .attach_current_thread()
+        .expect("failed to attach jvm thread")
+}
