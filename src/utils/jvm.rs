e2de34a4f5f06bedbf11be55818c5c737a5e87c3
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-09 01:34:47 +0800

    minor refactor

--- src/utils/jvm.rs
+++ src/utils/jvm.rs
@@ -2,7 +2,7 @@ use std::sync::{Arc, Once};
 
 use jni::{AttachGuard, InitArgsBuilder, JNIVersion, JavaVM, errors::Result, objects::JClass, signature::{ReturnType, Primitive}};
 
-pub fn jvm() -> &'static Arc<JavaVM> {
+fn jvm() -> &'static Arc<JavaVM> {
     static mut JVM: Option<Arc<JavaVM>> = None;
     static INIT: Once = Once::new();
 
@@ -29,14 +29,13 @@ pub fn attach_current_thread() -> AttachGuard<'static> {
         .expect("Failed to attach jvm thread")
 }
 
-pub fn get_class<'a>() -> Result<JClass<'a>> {
+fn get_class<'a>() -> Result<JClass<'a>> {
     attach_current_thread().find_class("java/lang/Class")
 }
 
 pub fn get_class_modifiers<S>(class_name: S) -> Result<u32> where S: Into<String> {
-    let class = get_class()?;
     let gaurd = attach_current_thread();
-    let method_id = gaurd.get_method_id(class, "getModifiers", "()I")?;
+    let method_id = gaurd.get_method_id(get_class()?, "getModifiers", "()I")?;
     let modifiers = gaurd.call_method_unchecked(gaurd.find_class(class_name.into())?, method_id, ReturnType::Primitive(Primitive::Int), &[])?;
 
     modifiers.i().map(|m| m as u32)
