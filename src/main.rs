449ff054b1eed450f2564ddd1c5dc25b9691e88e
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-08 16:56:42 +0800

    add modules

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-pub mod util;
+pub mod utils;
 mod label;
+mod edge;
+mod frame;
+mod error;
+pub mod opcodes;
+mod constants;
 
 extern crate jni;
 
 fn main() {
-    let mut env = util::attach_current_thread();
+    let mut env = utils::jvm::attach_current_thread();
 }
