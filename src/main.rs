03d22473451d4b44a779e00e7d9bf7b16853cc3e
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-08 18:22:19 +0800

    reorganize modules

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-pub mod utils;
-mod label;
-mod edge;
-mod frame;
-mod error;
-pub mod opcodes;
+pub mod byte_vec;
 mod constants;
+mod edge;
+mod error;
+mod frame;
+mod label;
+pub mod opcodes;
+pub mod utils;
 
 extern crate jni;
 
