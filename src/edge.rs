b1a17b75ee4b5017f1dc2170e89b321ffa0a6e53
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-08 18:39:34 +0800

    move edge constants to module level

--- src/edge.rs
+++ src/edge.rs
@@ -2,6 +2,9 @@ use std::rc::Rc;
 
 use crate::label::Label;
 
+pub(crate) const JUMP: u32 = 0;
+pub(crate) const EXCEPTION: u32 = 0x7FFFFFFF;
+
 pub(crate) struct Edge {
     pub(crate) info: i32,
     pub(crate) successor: Rc<dyn Label>,
@@ -9,8 +12,6 @@ pub(crate) struct Edge {
 }
 
 impl Edge {
-    pub(crate) const JUMP: u32 = 0;
-    pub(crate) const EXCEPTION: u32 = 0x7FFFFFFF;
 
     pub const fn new(info: i32, successor: Rc<dyn Label>, next_edge: Option<Rc<Edge>>) -> Self {
         Self{
