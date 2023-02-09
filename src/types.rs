623ec0d1fad4fd68cd49c0fcd7fc2242baed5f81
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-09 18:44:27 +0800

    add TypeRef

--- src/types.rs
+++ src/types.rs
@@ -194,98 +194,246 @@ impl ToString for TypePath {
     }
 }
 
-/// The sort of type references that target a type parameter of a generic class. See {@link
-/// #getSort}.
-pub const CLASS_TYPE_PARAMETER: u8 = 0x00;
+/// The sort of type references that target a type parameter of a generic class. See
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const CLASS_TYPE_PARAMETER: i32 = 0x00;
 
-/// The sort of type references that target a type parameter of a generic method. See {@link
-/// #getSort}.
-pub const METHOD_TYPE_PARAMETER: u8 = 0x01;
+/// The sort of type references that target a type parameter of a generic method. See
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const METHOD_TYPE_PARAMETER: i32 = 0x01;
 
 /// The sort of type references that target the super class of a class or one of the interfaces it
-/// implements. See {@link #getSort}.
-pub const CLASS_EXTENDS: u8 = 0x10;
+/// implements. See [`TypeRef::sort`](TypeRef#method.sort).
+pub const CLASS_EXTENDS: i32 = 0x10;
 
 /// The sort of type references that target a bound of a type parameter of a generic class. See
-/// {@link #getSort}.
-pub const CLASS_TYPE_PARAMETER_BOUND: u8 = 0x11;
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const CLASS_TYPE_PARAMETER_BOUND: i32 = 0x11;
 
 /// The sort of type references that target a bound of a type parameter of a generic method. See
-/// {@link #getSort}.
-pub const METHOD_TYPE_PARAMETER_BOUND: u8 = 0x12;
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const METHOD_TYPE_PARAMETER_BOUND: i32 = 0x12;
 
-/// The sort of type references that target the type of a field. See {@link #getSort}.
-pub const FIELD: u8 = 0x13;
+/// The sort of type references that target the type of a field. See
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const FIELD: i32 = 0x13;
 
-/// The sort of type references that target the return type of a method. See {@link #getSort}.
-pub const METHOD_RETURN: u8 = 0x14;
+/// The sort of type references that target the return type of a method. See
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const METHOD_RETURN: i32 = 0x14;
 
-/// The sort of type references that target the receiver type of a method. See {@link #getSort}.
-pub const METHOD_RECEIVER: u8 = 0x15;
+/// The sort of type references that target the receiver type of a method. See
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const METHOD_RECEIVER: i32 = 0x15;
 
-/// The sort of type references that target the type of a formal parameter of a method. See {@link
-/// #getSort}.
-pub const METHOD_FORMAL_PARAMETER: u8 = 0x16;
+/// The sort of type references that target the type of a formal parameter of a method. See
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const METHOD_FORMAL_PARAMETER: i32 = 0x16;
 
 /// The sort of type references that target the type of an exception declared in the throws clause
-/// of a method. See {@link #getSort}.
-pub const THROWS: u8 = 0x17;
+/// of a method. See [`TypeRef::sort`](TypeRef#method.sort).
+pub const THROWS: i32 = 0x17;
 
-/// The sort of type references that target the type of a local variable in a method. See {@link
-/// #getSort}.
-pub const LOCAL_VARIABLE: u8 = 0x40;
+/// The sort of type references that target the type of a local variable in a method. See
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const LOCAL_VARIABLE: i32 = 0x40;
 
-/// The sort of type references that target the type of a resource variable in a method. See {@link
-/// #getSort}.
-pub const RESOURCE_VARIABLE: u8 = 0x41;
+/// The sort of type references that target the type of a resource variable in a method. See
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const RESOURCE_VARIABLE: i32 = 0x41;
 
 /// The sort of type references that target the type of the exception of a 'catch' clause in a
-/// method. See {@link #getSort}.
-pub const EXCEPTION_PARAMETER: u8 = 0x42;
+/// method. See [`TypeRef::sort`](TypeRef#method.sort).
+pub const EXCEPTION_PARAMETER: i32 = 0x42;
 
 /// The sort of type references that target the type declared in an 'instanceof' instruction. See
-/// {@link #getSort}.
-pub const INSTANCEOF: u8 = 0x43;
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const INSTANCEOF: i32 = 0x43;
 
 /// The sort of type references that target the type of the object created by a 'new' instruction.
-/// See {@link #getSort}.
-pub const NEW: u8 = 0x44;
+/// See [`TypeRef::sort`](TypeRef#method.sort).
+pub const NEW: i32 = 0x44;
 
 /// The sort of type references that target the receiver type of a constructor reference. See
-/// {@link #getSort}.
-pub const CONSTRUCTOR_REFERENCE: u8 = 0x45;
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const CONSTRUCTOR_REFERENCE: i32 = 0x45;
 
-/// The sort of type references that target the receiver type of a method reference. See {@link
-/// #getSort}.
-pub const METHOD_REFERENCE: u8 = 0x46;
+/// The sort of type references that target the receiver type of a method reference. See
+/// [`TypeRef::sort`](TypeRef#method.sort).
+pub const METHOD_REFERENCE: i32 = 0x46;
 
 /// The sort of type references that target the type declared in an explicit or implicit cast
-/// instruction. See {@link #getSort}.
-pub const CAST: u8 = 0x47;
+/// instruction. See [`TypeRef::sort`](TypeRef#method.sort).
+pub const CAST: i32 = 0x47;
 
 /// The sort of type references that target a type parameter of a generic constructor in a
-/// constructor call. See {@link #getSort}.
-pub const CONSTRUCTOR_INVOCATION_TYPE_ARGUMENT: u8 = 0x48;
+/// constructor call. See [`TypeRef::sort`](TypeRef#method.sort).
+pub const CONSTRUCTOR_INVOCATION_TYPE_ARGUMENT: i32 = 0x48;
 
 /// The sort of type references that target a type parameter of a generic method in a method call.
-/// See {@link #getSort}.
-pub const METHOD_INVOCATION_TYPE_ARGUMENT: u8 = 0x49;
+/// See [`TypeRef::sort`](TypeRef#method.sort).
+pub const METHOD_INVOCATION_TYPE_ARGUMENT: i32 = 0x49;
 
 /// The sort of type references that target a type parameter of a generic constructor in a
-/// constructor reference. See {@link #getSort}.
-pub const CONSTRUCTOR_REFERENCE_TYPE_ARGUMENT: u8 = 0x4A;
+/// constructor reference. See [`TypeRef::sort`](TypeRef#method.sort).
+pub const CONSTRUCTOR_REFERENCE_TYPE_ARGUMENT: i32 = 0x4A;
 
 /// The sort of type references that target a type parameter of a generic method in a method
-/// reference. See {@link #getSort}.
-pub const METHOD_REFERENCE_TYPE_ARGUMENT: u8 = 0x4B;
+/// reference. See [`TypeRef::sort`](TypeRef#method.sort).
+pub const METHOD_REFERENCE_TYPE_ARGUMENT: i32 = 0x4B;
 
-pub trait TypeReferece {}
+/// A reference to a type appearing in a class, field or method declaration, or on an instruction.
+/// Such a reference designates the part of the class where the referenced type is appearing (e.g. an
+/// 'extends', 'implements' or 'throws' clause, a 'new' instruction, a 'catch' clause, a type cast, a
+/// local variable declaration, etc).
+///
+/// **author** Eric Bruneton
+pub struct TypeRef {
+    /// The target_type and target_info structures - as defined in the Java Virtual Machine
+    /// Specification (JVMS) - corresponding to this type reference. target_type uses one byte, and all
+    /// the target_info union fields use up to 3 bytes (except localvar_target, handled with the
+    /// specific method [`MethodVisitor::visit_local_variable_annotation`]
+    /// (crate::MethodVisitor#method.visit_local_variable_annotation)). Thus, both structures can
+    /// be stored in an int.
+    ///
+    /// <p>This int field stores target_type (called the TypeRef 'sort' in the public API of this
+    /// class) in its most significant byte, followed by the target_info fields. Depending on
+    /// target_type, 1, 2 or even 3 least significant bytes of this field are unused. target_info
+    /// fields which reference bytecode offsets are set to 0 (these offsets are ignored in ClassReader,
+    /// and recomputed in MethodWriter).
+    ///
+    /// ### See <a href="https://docs.oracle.com/javase/specs/jvms/se9/html/jvms-4.html#jvms-4.7.20">JVMS 4.7.20</a>
+    /// ### See <a href="https://docs.oracle.com/javase/specs/jvms/se9/html/jvms-4.html#jvms-4.7.20.1">JVMS 4.7.20.1</a>
+    target_type_and_info: i32,
+}
+
+impl TypeRef {
+    pub const fn new(type_ref: i32) -> Self {
+        Self {
+            target_type_and_info: type_ref,
+        }
+    }
+
+    pub const fn type_ref(sort: i32) -> Self {
+        Self::new(sort << 24)
+    }
+
+    pub const fn type_param_ref(sort: i32, param_index: i32) -> Self {
+        Self::new((sort << 24) | (param_index << 16))
+    }
+
+    pub const fn type_param_bound_ref(sort: i32, param_index: i32, bound_index: i32) -> Self {
+        Self::new((sort << 24) | (param_index << 16) | (bound_index << 8))
+    }
+
+    pub const fn super_type_ref(itf_index: i32) -> Self {
+        Self::new((CLASS_EXTENDS << 24) | ((itf_index & 0xFFFF) << 8))
+    }
+
+    pub const fn formal_param_ref(param_index: i32) -> Self {
+        Self::new((METHOD_FORMAL_PARAMETER << 24) | (param_index << 16))
+    }
+
+    pub const fn exception_ref(exception_index: i32) -> Self {
+        Self::new((THROWS << 24) | (exception_index << 8))
+    }
+
+    pub const fn try_catch_ref(try_catch_block_index: i32) -> Self {
+        Self::new((EXCEPTION_PARAMETER << 24) | (try_catch_block_index << 8))
+    }
+
+    pub const fn type_arg_ref(sort: i32, arg_index: i32) -> Self {
+        Self::new((sort << 24) | arg_index)
+    }
+
+    pub(crate) fn put_target<BV>(
+        target_type_and_info: i32,
+        output: &mut BV,
+    ) -> Result<(), KapiError>
+    where
+        BV: ByteVec,
+    {
+        match target_type_and_info {
+            CLASS_TYPE_PARAMETER | METHOD_TYPE_PARAMETER | METHOD_FORMAL_PARAMETER => {
+                output.put_short((target_type_and_info >> 16) as i16)
+            }
+            FIELD | METHOD_RETURN | METHOD_RECEIVER => {
+                output.put_short((target_type_and_info >> 16) as i16)
+            }
+            CAST
+            | CONSTRUCTOR_INVOCATION_TYPE_ARGUMENT
+            | METHOD_INVOCATION_TYPE_ARGUMENT
+            | CONSTRUCTOR_REFERENCE_TYPE_ARGUMENT
+            | METHOD_REFERENCE_TYPE_ARGUMENT => output.put_short(target_type_and_info as i16),
+            CLASS_EXTENDS
+            | CLASS_TYPE_PARAMETER_BOUND
+            | METHOD_TYPE_PARAMETER_BOUND
+            | THROWS
+            | EXCEPTION_PARAMETER
+            | INSTANCEOF
+            | NEW
+            | CONSTRUCTOR_REFERENCE
+            | METHOD_REFERENCE => {
+                output.put_byte((target_type_and_info >> 24) as i8);
+                output.put_short(((target_type_and_info & 0xFFFF00) >> 8) as i16);
+            }
+            _ => {
+                return Err(KapiError::ArgError(String::from(
+                    "Illegal type reference target",
+                )))
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn sort(&self) -> i32 {
+        self.target_type_and_info >> 24
+    }
+
+    pub fn type_param_index(&self) -> i32 {
+        (self.target_type_and_info & 0x00FF0000) >> 16
+    }
+
+    pub fn type_param_bound_index(&self) -> i32 {
+        (self.target_type_and_info & 0x0000FF00) >> 8
+    }
+
+    pub fn super_type_index(&self) -> i32 {
+        ((self.target_type_and_info & 0x00FFFF00) >> 8) as i16 as i32
+    }
+
+    pub fn formal_param_index(&self) -> i32 {
+        (self.target_type_and_info & 0x00FF0000) >> 16
+    }
+
+    pub fn exception_index(&self) -> i32 {
+        (self.target_type_and_info & 0x00FFFF00) >> 8
+    }
+
+    pub fn try_catch_block_index(&self) -> i32 {
+        (self.target_type_and_info & 0x00FFFF00) >> 8
+    }
+
+    pub fn type_arg_index(&self) -> i32 {
+        self.target_type_and_info & 0xFF
+    }
+
+    pub fn value(&self) -> i32 {
+        self.target_type_and_info
+    }
+}
 
 #[cfg(test)]
 mod test {
     use std::str::FromStr;
 
-    use crate::types::{self, TypePath};
+    use crate::types::{self, TypePath, THROWS};
+
+    use super::{
+        TypeRef, CAST, CLASS_EXTENDS, CLASS_TYPE_PARAMETER, EXCEPTION_PARAMETER, FIELD,
+        METHOD_FORMAL_PARAMETER,
+    };
 
     #[test]
     fn test_type_path_len() {
@@ -316,5 +464,70 @@ mod test {
         assert!(TypePath::from_str("-").is_err());
         assert!(TypePath::from_str("=").is_err());
         assert!(TypePath::from_str("1-").is_err());
+    }
+
+    #[test]
+    fn test_new_type_ref() {
+        let type_ref = TypeRef::type_ref(FIELD);
+
+        assert_eq!(FIELD, type_ref.sort());
+        assert_eq!(FIELD << 24, type_ref.value());
+    }
+
+    #[test]
+    fn test_new_type_param_ref() {
+        let type_param_ref = TypeRef::type_param_ref(CLASS_TYPE_PARAMETER, 3);
+
+        assert_eq!(CLASS_TYPE_PARAMETER, type_param_ref.sort());
+        assert_eq!(3, type_param_ref.type_param_index());
+    }
+
+    #[test]
+    fn test_new_type_param_bound_ref() {
+        let type_param_bound_ref = TypeRef::type_param_bound_ref(CLASS_TYPE_PARAMETER, 3, 7);
+
+        assert_eq!(CLASS_TYPE_PARAMETER, type_param_bound_ref.sort());
+        assert_eq!(3, type_param_bound_ref.type_param_index());
+        assert_eq!(7, type_param_bound_ref.type_param_bound_index());
+    }
+
+    #[test]
+    fn test_new_super_type_ref() {
+        let super_type_ref = TypeRef::super_type_ref(-1);
+
+        assert_eq!(CLASS_EXTENDS, super_type_ref.sort());
+        assert_eq!(-1, super_type_ref.super_type_index());
+    }
+
+    #[test]
+    fn test_new_formal_type_ref() {
+        let formal_type_ref = TypeRef::formal_param_ref(3);
+
+        assert_eq!(METHOD_FORMAL_PARAMETER, formal_type_ref.sort());
+        assert_eq!(3, formal_type_ref.formal_param_index());
+    }
+
+    #[test]
+    fn test_new_exception_ref() {
+        let exception_ref = TypeRef::exception_ref(3);
+
+        assert_eq!(THROWS, exception_ref.sort());
+        assert_eq!(3, exception_ref.exception_index());
+    }
+
+    #[test]
+    fn test_try_catch_ref() {
+        let try_catch_ref = TypeRef::try_catch_ref(3);
+
+        assert_eq!(EXCEPTION_PARAMETER, try_catch_ref.sort());
+        assert_eq!(3, try_catch_ref.try_catch_block_index());
+    }
+
+    #[test]
+    fn test_type_arg_ref() {
+        let type_arg_ref = TypeRef::type_arg_ref(CAST, 3);
+
+        assert_eq!(CAST, type_arg_ref.sort());
+        assert_eq!(3, type_arg_ref.type_arg_index());
     }
 }
