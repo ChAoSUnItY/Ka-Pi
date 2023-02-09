fb860c435372f55682f0a9a50fa9b753c481e3b7
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-10 01:16:38 +0800

    Add more fields and make class cachable

--- src/class.rs
+++ src/class.rs
@@ -1,13 +1,25 @@
-use jni::objects::JClass;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+use jni::objects::GlobalRef;
+use lazy_static::lazy_static;
 
 use crate::class::LazyClassMember::Failed;
 use crate::error::KapiError;
-use crate::types::Type;
-use crate::utils::jvm::{get_class, get_class_modifiers};
+use crate::types::canonical_to_internal;
+use crate::utils::jvm::{as_global_ref, get_class, get_class_modifiers};
+
+lazy_static! {
+    static ref CLASS_CACHE: Mutex<HashMap<String, Arc<Class>>> = Mutex::new(HashMap::new());
+}
 
 /// Simple representation of lazy initialized class member, to avoid huge cost of communication between
 /// Rust and JVM. See [`Class`].
-enum LazyClassMember<T> {
+#[derive(Debug, Eq, PartialEq)]
+enum LazyClassMember<T>
+where
+    T: Eq + PartialEq,
+{
     /// Represents the data had been successfully invoked and returned from JVM.
     Initialized(T),
     /// Represents the data request invocation is failed.
@@ -18,47 +30,138 @@ enum LazyClassMember<T> {
 
 /// This is a lazy representation of Class<?>, to simplify the work of interop with [`Type`].
 ///
-/// All class data are lazily initialized to avoid huge cost of communication between Rust and JVM. 
-pub struct Class<'a> {
-    pub class_path: String,
-    pub class: JClass<'a>,
+/// All class data are lazily initialized to avoid huge cost of communication between Rust and JVM.
+#[derive(Debug)]
+pub struct Class {
+    /// Represents class' full qualified path with a 'L' prefixed and ';' suffixed, if class is an
+    /// object class; otherwise, represents primitive type's actual name which has only 1 character.
+    internal_name: String,
+    class: GlobalRef,
+    /// Represents array type's component class type.
+    component_class: Option<Arc<Class>>,
+    // descriptor: String,
     modifiers: LazyClassMember<u32>,
 }
 
-impl<'a> Class<'a> {
-    pub fn get_class<S>(class_path: S) -> Result<Self, KapiError> where S: Into<String> {
-        let path_str = class_path.into();
+impl Class {
+    pub fn get_class<S>(canonical_name: S) -> Result<Arc<Self>, KapiError>
+    where
+        S: Into<String>,
+    {
+        let internal_name = canonical_to_internal(canonical_name);
+        let class_cache = CLASS_CACHE.lock().unwrap();
 
-        if let Ok(jclass) = get_class(&path_str) {
-            let class = Self::new(path_str.clone(), jclass);
-
-            Ok(class)
+        if let Some(class) = class_cache.get::<String>(&internal_name) {
+            Ok(class.clone())
         } else {
-            Err(KapiError::ClassResolveError(format!("Unable to resolve class {}", path_str)))
+            Self::resolve_class(internal_name)
         }
     }
 
-    const fn new(class_path: String, class: JClass<'a>) -> Self {
+    fn resolve_class<S>(internal_name: S) -> Result<Arc<Self>, KapiError>
+    where
+        S: Into<String>,
+    {
+        let internal_name_str = internal_name.into();
+
+        if let Ok(global_ref) =
+            get_class(&internal_name_str).and_then(|class| as_global_ref(class.into()))
+        {
+            if internal_name_str.ends_with("[]") {
+                let component_class =
+                    Self::resolve_class(internal_name_str.trim_end_matches("[]"))?;
+                let class = Arc::new(Self::new(
+                    internal_name_str.clone(),
+                    global_ref,
+                    Some(component_class),
+                ));
+                let mut cache = CLASS_CACHE.lock().unwrap();
+
+                cache.insert(internal_name_str, class.clone());
+
+                Ok(class)
+            } else {
+                let class = Arc::new(Self::new(internal_name_str.clone(), global_ref, None));
+                let mut cache = CLASS_CACHE.lock().unwrap();
+
+                cache.insert(internal_name_str.into(), class.clone());
+
+                Ok(class)
+            }
+        } else {
+            Err(KapiError::ClassResolveError(format!(
+                "Unable to resolve class {}",
+                internal_name_str
+            )))
+        }
+    }
+
+    const fn new(
+        internal_name: String,
+        class: GlobalRef,
+        component_class: Option<Arc<Self>>,
+    ) -> Self {
         Self {
-            class_path,
+            internal_name,
             class,
+            component_class,
             modifiers: LazyClassMember::Uninitialized,
         }
     }
 
+    /// Returns array type's component class type. If class is not an array type, then returns
+    /// [`None`] instead.
+    // pub fn component_class(&self) -> &Option<Box<Class>> {
+    //     &self.component_class
+    // }
+
+    /// Returns class type's modifiers.
     pub fn modifiers(&mut self) -> Result<u32, KapiError> {
         if let LazyClassMember::Initialized(modifiers) = self.modifiers {
             Ok(modifiers)
-        } else if let Ok(modifiers) = get_class_modifiers(&self.class_path) {
+        } else if let Ok(modifiers) = get_class_modifiers(&self.internal_name) {
             self.modifiers = LazyClassMember::Initialized(modifiers);
 
             Ok(modifiers)
         } else {
-            let err = KapiError::ClassResolveError(format!("Unable to resolve modifiers of class {}", self.class_path));
+            let err = KapiError::ClassResolveError(format!(
+                "Unable to resolve modifiers of class {}",
+                self.internal_name
+            ));
 
             self.modifiers = Failed(err.clone());
 
             Err(err)
         }
+    }
+}
+
+impl PartialEq for Class {
+    fn eq(&self, other: &Self) -> bool {
+        self.internal_name == other.internal_name
+            && self.component_class == other.component_class
+            && self.modifiers == other.modifiers
+    }
+}
+
+impl Eq for Class {}
+
+#[cfg(test)]
+mod test {
+    use crate::class::Class;
+    use crate::utils::jvm::attach_current_thread;
+
+    #[test]
+    fn test_get_class() {
+        let _ = attach_current_thread();
+
+        let string_class = Class::get_class("java.lang.String");
+
+        assert!(string_class.is_ok());
+
+        let cached_string_class = Class::get_class("java.lang.String");
+
+        assert!(cached_string_class.is_ok());
+        assert_eq!(*string_class.unwrap(), *cached_string_class.unwrap());
     }
 }
