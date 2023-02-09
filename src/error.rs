5435b1824b04b85115236d60c2762f9b9a1e0bd0
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-09 14:57:37 +0800

    convert erros to tuple

--- src/error.rs
+++ src/error.rs
@@ -2,15 +2,19 @@ use std::{error::Error, fmt::Display};
 
 #[derive(Debug)]
 pub enum RasmError {
-    StateError { cause: &'static str },
-    Utf8Error { cause: &'static str },
+    StateError(&'static str),
+    Utf8Error(&'static str),
+    TypeError(&'static str),
+    ArgError(&'static str),
 }
 
 impl Display for RasmError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
-            RasmError::StateError { cause } => write!(f, "{}", cause),
-            RasmError::Utf8Error { cause } => write!(f, "{}", cause),
+            RasmError::StateError(cause) => write!(f, "{}", cause),
+            RasmError::Utf8Error(cause) => write!(f, "{}", cause),
+            RasmError::TypeError(cause) => write!(f, "{}", cause),
+            RasmError::ArgError(cause) => write!(f, "{}", cause),
         }
     }
 }
