77f03a00fc8f96136709bc108808a30d7128accb
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-10 16:20:17 +0800

    Introduce invoke_method to simplify invocation

--- src/jvm.rs
+++ src/jvm.rs
@@ -12,7 +12,8 @@ use std::fmt::Formatter;
 use std::rc::Rc;
 use std::sync::{Arc, Once};
 
-use jni::objects::{GlobalRef, JObject};
+use jni::objects::{GlobalRef, JObject, JValue};
+use jni::sys::jvalue;
 use jni::{
     errors::Result,
     objects::JClass,
@@ -23,7 +24,9 @@ use jni::{
 use crate::class::{Class, Method};
 
 pub trait FromVMState<'a> {
-    fn from(vm_state: &Rc<RefCell<PseudoVMState<'a>>>, j_object: JObject<'a>) -> Self;
+    fn from(vm_state: Rc<RefCell<PseudoVMState<'a>>>, j_object: JObject<'a>) -> Result<Self>
+    where
+        Self: Sized;
 }
 
 /// A [`PseudoVMState`] represents an intermediate communication bridge between Rust and JVM, which
@@ -87,12 +90,35 @@ fn get_clazz<'a>() -> Result<JClass<'a>> {
 }
 
 /// Get a [`JClass`](JClass) from current JVM environment.
-pub(crate) fn get_class<'a>(vm_state: Rc<RefCell<PseudoVMState<'a>>>, class_name: &String) -> Result<JClass<'a>> {
+pub(crate) fn get_class<'a>(
+    vm_state: Rc<RefCell<PseudoVMState<'a>>>,
+    class_name: &String,
+) -> Result<JClass<'a>> {
     vm_state.borrow().attach_guard.find_class(class_name)
 }
 
-pub(crate) fn as_global_ref(vm_state: Rc<RefCell<PseudoVMState>>, obj: JObject) -> Result<GlobalRef> {
+pub(crate) fn as_global_ref(
+    vm_state: Rc<RefCell<PseudoVMState>>,
+    obj: JObject,
+) -> Result<GlobalRef> {
     vm_state.borrow().attach_guard.new_global_ref(obj)
+}
+
+pub(crate) fn invoke_method<'a, S1, S2>(
+    vm_state: Rc<RefCell<PseudoVMState<'a>>>,
+    class: &JClass,
+    name: S1,
+    sig: S2,
+    args: &[jvalue],
+    return_type: ReturnType,
+) -> Result<JValue<'a>>
+where
+    S1: Into<String>,
+    S2: Into<String>,
+{
+    let guard = &vm_state.borrow().attach_guard;
+    let method_id = guard.get_method_id(*class, name.into(), sig.into())?;
+    guard.call_method_unchecked(*class, method_id, return_type, args)
 }
 
 /// Get a [`u32`] represents modifiers applied on class.
@@ -100,29 +126,36 @@ pub(crate) fn as_global_ref(vm_state: Rc<RefCell<PseudoVMState>>, obj: JObject) 
 /// The returned [`u32`] is a bitset to represents combination of access modifiers.
 ///
 /// ## See [JVM 4.1 Table 4.1](https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.1)
-pub(crate) fn get_class_modifiers(vm_state: Rc<RefCell<PseudoVMState>>, class: &JClass) -> Result<u32> {
-    let guard = &vm_state.borrow().attach_guard;
-    let method_id = guard.get_method_id(get_clazz()?, "getModifiers", "()I")?;
-    let modifiers = guard.call_method_unchecked(
-        *class,
-        method_id,
-        ReturnType::Primitive(Primitive::Int),
+pub(crate) fn get_class_modifiers(
+    vm_state: Rc<RefCell<PseudoVMState>>,
+    class: &JClass,
+) -> Result<u32> {
+    invoke_method(
+        vm_state,
+        class,
+        "getModifiers",
+        "()I",
         &[],
-    )?;
-
-    modifiers.i().map(|i| i as u32)
+        ReturnType::Primitive(Primitive::Int),
+    )?
+    .i()
+    .map(|i| i as u32)
 }
 
-pub(crate) fn get_class_declared_methods<'a>(vm_state: Rc<RefCell<PseudoVMState>>, class: &JClass) -> Result<Vec<Rc<Method<'a>>>> {
-    let guard = &vm_state.borrow().attach_guard;
-    let method_id = guard.get_method_id(
-        get_clazz()?,
+pub(crate) fn get_class_declared_methods<'a>(
+    vm_state: Rc<RefCell<PseudoVMState>>,
+    class: &JClass,
+) -> Result<Vec<Rc<Method<'a>>>> {
+    let declared_methods_obj = invoke_method(
+        vm_state,
+        class,
         "getDeclaredMethods",
         "()[java/lang/reflect/Method;",
-    )?;
-    let declared_methods_obj = guard
-        .call_method_unchecked(*class, method_id, ReturnType::Array, &[])?
-        .l()?;
+        &[],
+        ReturnType::Array,
+    )?
+    .l()?;
+    let guard = &vm_state.borrow().attach_guard;
     let len = guard.get_array_length(declared_methods_obj.cast())?;
     let mut declared_methods = Vec::with_capacity(len as usize);
 
