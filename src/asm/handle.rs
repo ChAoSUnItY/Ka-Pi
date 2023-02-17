3b1d301ceae0750612880bde28b068178b406414
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-18 01:08:08 +0800

    add Handle

---
+++ src/asm/handle.rs
@@ -0,0 +1,23 @@
+use super::opcodes;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Handle {
+    tag: u8,
+    owner: String,
+    name: String,
+    descriptor: String,
+    is_interface: bool,
+}
+
+impl Handle {
+    pub const fn new(tag: u8, owner: String, name: String, descriptor: String) -> Self
+    {
+        Self {
+            tag,
+            owner,
+            name,
+            descriptor,
+            is_interface: tag == opcodes::H_INVOKEINTERFACE,
+        }
+    }
+}
