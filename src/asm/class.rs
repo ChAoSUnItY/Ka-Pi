0d4c4917ee3e842fc2a48dad808246e8e0900aeb
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-23 00:11:19 +0800

    Add missing match arms for function `read_const`

--- src/asm/class.rs
+++ src/asm/class.rs
@@ -3,20 +3,29 @@ use std::rc::Rc;
 
 use crate::asm::constants::{ConstantDynamic, ConstantObject};
 use crate::asm::{symbol, Handle};
+use crate::asm::types::Type;
 use crate::error::{KapiError, KapiResult};
 use crate::utils::PushReturn;
 
 pub(crate) const EXPAND_ASM_INSNS: u8 = 256u16 as u8;
 
 pub trait Reader {
+    #[inline]
     fn bytes(&self) -> &Vec<u8>;
+    #[inline]
     fn header(&self) -> usize;
+    #[inline]
     fn constant_pool_info_offsets(&self) -> &Vec<usize>;
+    #[inline]
     fn constant_utf8_values(&self) -> &Vec<Rc<String>>;
+    #[inline]
     fn constant_dynamic_values(&self) -> &Vec<Rc<ConstantDynamic>>;
+    #[inline]
     fn bootstrap_method_offsets(&self) -> &Vec<usize>;
 
+    #[inline]
     fn put_utf8(&mut self, utf8_string: Rc<String>, entry_index: usize);
+    #[inline]
     fn put_constant_dynamic(&mut self, constant_dynamic: Rc<ConstantDynamic>, entry_index: usize);
 
     fn slice_rev<const SIZE: usize>(&self, offset: usize) -> [u8; SIZE] {
@@ -61,6 +70,7 @@ pub trait Reader {
         f64::from_ne_bytes(self.slice_rev(offset))
     }
 
+    #[inline]
     fn read_utf8(&mut self, offset: usize) -> KapiResult<Rc<String>> {
         let entry_index = self.read_u16(offset) as usize;
 
@@ -71,6 +81,7 @@ pub trait Reader {
         }
     }
 
+    #[inline]
     fn read_utf8_from_cache(&mut self, entry_index: usize) -> KapiResult<Rc<String>> {
         if let Some(entry) = self.constant_utf8_values().get(entry_index) {
             Ok(entry.clone())
@@ -85,24 +96,29 @@ pub trait Reader {
         }
     }
 
+    #[inline]
     fn read_utf8_from_bytes(&self, data_offset: usize, len: usize) -> KapiResult<String> {
         let utf8_bytes = &self.bytes()[data_offset..data_offset + len];
 
         Ok(String::from_utf8_lossy(utf8_bytes).into())
     }
 
+    #[inline]
     fn read_stringish(&mut self, offset: usize) -> KapiResult<Rc<String>> {
         self.read_utf8(self.constant_pool_info_offsets()[self.read_u16(offset) as usize])
     }
 
+    #[inline]
     fn read_class(&mut self, offset: usize) -> KapiResult<Rc<String>> {
         self.read_stringish(offset)
     }
 
+    #[inline]
     fn read_module(&mut self, offset: usize) -> KapiResult<Rc<String>> {
         self.read_stringish(offset)
     }
 
+    #[inline]
     fn read_package(&mut self, offset: usize) -> KapiResult<Rc<String>> {
         self.read_stringish(offset)
     }
@@ -157,8 +173,20 @@ pub trait Reader {
             symbol::CONSTANT_LONG_TAG => Ok(Box::new(self.read_i64(info_offset))),
             symbol::CONSTANT_FLOAT_TAG => Ok(Box::new(self.read_f32(info_offset))),
             symbol::CONSTANT_DOUBLE_TAG => Ok(Box::new(self.read_f64(info_offset))),
-            // symbol::CONSTANT_CLASS_TAG => Ok(Box::new(self.read_utf8(info_offset))),
+            symbol::CONSTANT_CLASS_TAG => Ok(Box::new(Type::object_type_from_string(self.read_utf8(info_offset)?.to_string()))),
             symbol::CONSTANT_STRING_TAG => Ok(Box::new(self.read_utf8(info_offset)?)),
+            symbol::CONSTANT_METHOD_TYPE_TAG => Ok(Box::new(Type::method_type_from_string(self.read_utf8(info_offset)?.to_string()))),
+            symbol::CONSTANT_METHOD_HANDLE_TAG => {
+                let reference_kind = self.read_u8(info_offset);
+                let reference_info_offset = self.constant_pool_info_offsets()[self.read_u16(info_offset + 1) as usize];
+                let name_and_type_info_offset = self.constant_pool_info_offsets()[self.read_u16(info_offset + 2) as usize];
+                let owner = self.read_class(reference_info_offset)?;
+                let descriptor = self.read_class(name_and_type_info_offset)?;
+                let is_interface = self.bytes()[reference_info_offset - 1] == symbol::CONSTANT_INTERFACE_METHODREF_TAG;
+                
+                Ok(Box::new(Handle::new(reference_kind, owner.to_string(), descriptor.to_string(), is_interface.to_string())))
+            },
+            symbol::CONSTANT_DYNAMIC_TAG => Ok(Box::from(self.read_constant_dynamic(info_offset)?)),
             _ => Err(KapiError::ArgError(format!(
                 "Illegal constant object at constant pool index {}",
                 entry_index
@@ -195,34 +223,42 @@ impl ClassReader {
 }
 
 impl Reader for ClassReader {
+    #[inline]
     fn bytes(&self) -> &Vec<u8> {
         &self.class_file_buffer
     }
 
+    #[inline]
     fn header(&self) -> usize {
         self.header
     }
 
+    #[inline]
     fn constant_pool_info_offsets(&self) -> &Vec<usize> {
         &self.constant_pool_info_offsets
     }
 
+    #[inline]
     fn constant_utf8_values(&self) -> &Vec<Rc<String>> {
         &self.constant_utf8_values
     }
 
+    #[inline]
     fn constant_dynamic_values(&self) -> &Vec<Rc<ConstantDynamic>> {
         &self.constant_dynamic_values
     }
 
+    #[inline]
     fn bootstrap_method_offsets(&self) -> &Vec<usize> {
         &self.bootstrap_method_offsets
     }
 
+    #[inline]
     fn put_utf8(&mut self, utf8_string: Rc<String>, _: usize) {
         self.constant_utf8_values.push(utf8_string)
     }
 
+    #[inline]
     fn put_constant_dynamic(&mut self, constant_dynamic: Rc<ConstantDynamic>, _: usize) {
         self.constant_dynamic_values.push(constant_dynamic)
     }
