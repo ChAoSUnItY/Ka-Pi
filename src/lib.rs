b46fd219cf87657b4ce144d2921ede5a5e3c130c
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-22 22:55:37 +0800

    Add #![no_std] for future rework usage

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! # Ka-Pi
 //! ### A JVM Bytecode Manipulation Framework inspired by ASM.
 //!
+#[cfg_attr(not(feature = "reflection"), no_std)] // This would very likely break the whole project, we'll enable this after first stable version 
 
 #[allow(unused)]
 pub mod asm;
