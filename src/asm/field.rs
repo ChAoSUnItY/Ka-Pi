2b6a18d217d7e2c71e096523de0ec84d9e252b34
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-24 05:21:08 +0800

    Implement `FieldVisitor`

--- src/asm/field.rs
+++ src/asm/field.rs
@@ -1,1 +1,23 @@
-pub trait FieldVisitor {}
+use crate::asm::annotation::AnnotationVisitor;
+use crate::asm::attribute::Attribute;
+use crate::asm::types::TypePath;
+
+#[allow(unused_variables)]
+pub trait FieldVisitor {
+    fn visit_annotation(&mut self, descriptor: String, visible: bool) -> Option<Box<dyn AnnotationVisitor>>
+    {
+        None
+    }
+    fn visit_type_annotation(
+        &mut self,
+        type_ref: i32,
+        type_path: &TypePath,
+        descriptor: String,
+        visible: bool,
+    ) -> Option<Box<dyn AnnotationVisitor>>
+    {
+        None
+    }
+    fn visit_attribute(&mut self, attribute: Box<dyn Attribute>) {}
+    fn visit_end(self) where Self: Sized {}
+}
