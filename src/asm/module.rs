34e65629c66a901a89444adb1dafdc3fb28aa95e
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-02-24 05:28:31 +0800

    Implement `ModuleVisitor`

--- src/asm/module.rs
+++ src/asm/module.rs
@@ -1,1 +1,10 @@
-pub trait ModuleVisitor {}
+pub trait ModuleVisitor {
+    fn visit_main_class(&mut self, main_class: String) {}
+    fn visit_package(&mut self, package: String) {}
+    fn visit_required(&mut self, module: String, access: u32, version: String) {}
+    fn visit_export(&mut self, package: String, access: u32, modules: &[String]) {}
+    fn visit_open(&mut self, package: String, access: u32, modules: &[String]) {}
+    fn visit_use(&mut self, service: String) {}
+    fn visit_provide(&mut self, service: String, provides: &[String]) {}
+    fn visit_end(self) where Self: Sized {}
+}
