de3790398aae236bb053e5f289e5fbae39bcc774
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-03-04 17:11:28 +0800

    Implement `AnnotationVisitor`

--- src/asm/annotation.rs
+++ src/asm/annotation.rs
@@ -1,1 +1,20 @@
-pub trait AnnotationVisitor {}
+use crate::asm::constants::ConstantObject;
+
+pub trait AnnotationVisitor {
+    fn visit(&mut self, name: &String, value: &dyn ConstantObject) {}
+    
+    fn visit_enum(&mut self, name: &String, descriptor: &String, value: &String) {}
+    
+    fn visit_annotation(&mut self, name: &String, descriptor: &String) -> Box<dyn AnnotationVisitor + '_> {
+        Box::new(AnnotationVisitorImpl::default())
+    }
+    
+    fn visit_array(&mut self, name: &String) {}
+    
+    fn visit_end(&mut self) {}
+}
+
+#[derive(Debug, Default)]
+pub struct AnnotationVisitorImpl {}
+
+impl AnnotationVisitor for AnnotationVisitorImpl {}
