6ed8048830b04e4669c6b81d733db2c7d20b7ddb
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-04-24 04:41:56 +0800

    Prepare for branching safety mechanism algorithm

--- src/asm/label.rs
+++ src/asm/label.rs
@@ -2,16 +2,20 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 #[derive(Debug, Clone)]
-pub struct Label(pub(crate) u32);
-
-impl Default for Label {
-    fn default() -> Self {
-        Self(0)
-    }
+pub struct Label {
+    pub(crate) destination_pos: u32,
+    current_stack_size: usize,
 }
 
 impl Label {
-    pub fn new_label() -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Self::default()))
+    pub fn new_label(current_stack_size: usize) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            destination_pos: 0,
+            current_stack_size
+        }))
+    }
+    
+    pub(crate) fn set_offset(&mut self, destination_pos: u32) {
+        self.destination_pos = destination_pos;
     }
 }
