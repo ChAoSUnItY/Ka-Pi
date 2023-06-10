a0bd8798a6e1ce0136730890d76600d235c3c91d
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-06-10 22:14:46 +0800

    Feat: Add constant access for attributes (part 2)

--- src/asm/node/attribute/module.rs
+++ src/asm/node/attribute/module.rs
@@ -1,14 +1,41 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
 use crate::asm::node::access_flag::{ExportsAccessFlag, OpensAccessFlag, RequiresAccessFlag};
+use crate::asm::node::constant::{Constant, ConstantPool, Module, Package, Utf8};
 use crate::asm::node::ConstantRearrangeable;
 use crate::error::KapiResult;
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Requires {
     pub requires_index: u16,
     pub requires_flags: Vec<RequiresAccessFlag>,
     pub requires_version_index: u16,
+}
+
+impl Requires {
+    pub fn requires<'attribute, 'constant_pool: 'attribute>(
+        &'attribute self,
+        constant_pool: &'constant_pool ConstantPool,
+    ) -> Option<&'constant_pool Module> {
+        if let Some(Constant::Module(module)) = constant_pool.get(self.requires_index) {
+            Some(module)
+        } else {
+            None
+        }
+    }
+
+    pub fn requires_version<'attribute, 'constant_pool: 'attribute>(
+        &'attribute self,
+        constant_pool: &'constant_pool ConstantPool,
+    ) -> Option<&'constant_pool Utf8> {
+        if let Some(Constant::Utf8(utf8)) = constant_pool.get(self.requires_version_index) {
+            Some(utf8)
+        } else {
+            None
+        }
+    }
 }
 
 impl ConstantRearrangeable for Requires {
@@ -26,6 +53,36 @@ pub struct Exports {
     pub exports_flags: Vec<ExportsAccessFlag>,
     pub exports_to_count: u16,
     pub exports_to_index: Vec<u16>,
+}
+
+impl Exports {
+    pub fn exports<'attribute, 'constant_pool: 'attribute>(
+        &'attribute self,
+        constant_pool: &'constant_pool ConstantPool,
+    ) -> Option<&'constant_pool Package> {
+        if let Some(Constant::Package(package)) = constant_pool.get(self.exports_index) {
+            Some(package)
+        } else {
+            None
+        }
+    }
+
+    pub fn exports_to<'attribute, 'constant_pool: 'attribute>(
+        &'attribute self,
+        index: usize,
+        constant_pool: &'constant_pool ConstantPool,
+    ) -> Option<&'constant_pool Module> {
+        if let Some(Constant::Module(module)) = self
+            .exports_to_index
+            .get(index)
+            .map(|exports_to_index| constant_pool.get(*exports_to_index))
+            .flatten()
+        {
+            Some(module)
+        } else {
+            None
+        }
+    }
 }
 
 impl ConstantRearrangeable for Exports {
@@ -48,6 +105,36 @@ pub struct Opens {
     pub opens_to_index: Vec<u16>,
 }
 
+impl Opens {
+    pub fn opens<'attribute, 'constant_pool: 'attribute>(
+        &'attribute self,
+        constant_pool: &'constant_pool ConstantPool,
+    ) -> Option<&'constant_pool Package> {
+        if let Some(Constant::Package(package)) = constant_pool.get(self.opens_index) {
+            Some(package)
+        } else {
+            None
+        }
+    }
+
+    pub fn opens_to<'attribute, 'constant_pool: 'attribute>(
+        &'attribute self,
+        index: usize,
+        constant_pool: &'constant_pool ConstantPool,
+    ) -> Option<&'constant_pool Module> {
+        if let Some(Constant::Module(module)) = self
+            .opens_to_index
+            .get(index)
+            .map(|opens_to_index| constant_pool.get(*opens_to_index))
+            .flatten()
+        {
+            Some(module)
+        } else {
+            None
+        }
+    }
+}
+
 impl ConstantRearrangeable for Opens {
     fn rearrange(&mut self, rearrangements: &HashMap<u16, u16>) -> KapiResult<()> {
         Self::rearrange_index(&mut self.opens_index, rearrangements);
@@ -65,6 +152,36 @@ pub struct Provides {
     pub provides_index: u16,
     pub provides_with_count: u16,
     pub provides_with_index: Vec<u16>,
+}
+
+impl Provides {
+    pub fn provides<'attribute, 'constant_pool: 'attribute>(
+        &'attribute self,
+        constant_pool: &'constant_pool ConstantPool,
+    ) -> Option<&'constant_pool Package> {
+        if let Some(Constant::Package(package)) = constant_pool.get(self.provides_index) {
+            Some(package)
+        } else {
+            None
+        }
+    }
+
+    pub fn provides_to<'attribute, 'constant_pool: 'attribute>(
+        &'attribute self,
+        index: usize,
+        constant_pool: &'constant_pool ConstantPool,
+    ) -> Option<&'constant_pool Module> {
+        if let Some(Constant::Module(module)) = self
+            .provides_with_index
+            .get(index)
+            .map(|provides_with_index| constant_pool.get(*provides_with_index))
+            .flatten()
+        {
+            Some(module)
+        } else {
+            None
+        }
+    }
 }
 
 impl ConstantRearrangeable for Provides {
