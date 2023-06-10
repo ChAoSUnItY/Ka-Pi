2372f6174cedc893d7641352a7ab01d9137d9f99
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-06-11 00:15:54 +0800

    Feat: Add constant access functions to `Method`

--- src/asm/node/method.rs
+++ src/asm/node/method.rs
@@ -1,7 +1,12 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 use crate::asm::node::access_flag::MethodAccessFlag;
 use crate::asm::node::attribute::AttributeInfo;
+use crate::asm::node::constant::{Constant, ConstantPool, Utf8};
+use crate::asm::node::ConstantRearrangeable;
+use crate::error::KapiResult;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Method {
@@ -10,4 +15,43 @@ pub struct Method {
     pub descriptor_index: u16,
     pub attribute_infos_len: u16,
     pub attribute_infos: Vec<AttributeInfo>,
+}
+
+//noinspection DuplicatedCode
+impl Method {
+    pub fn name<'method, 'constant_pool: 'method>(
+        &'method self,
+        constant_pool: &'constant_pool ConstantPool,
+    ) -> Option<&'constant_pool Utf8> {
+        if let Some(Constant::Utf8(utf8)) = constant_pool.get(self.name_index) {
+            Some(utf8)
+        } else {
+            None
+        }
+    }
+
+    pub fn descriptor<'method, 'constant_pool: 'method>(
+        &'method self,
+        constant_pool: &'constant_pool ConstantPool,
+    ) -> Option<&'constant_pool Utf8> {
+        if let Some(Constant::Utf8(utf8)) = constant_pool.get(self.descriptor_index) {
+            Some(utf8)
+        } else {
+            None
+        }
+    }
+}
+
+//noinspection DuplicatedCode
+impl ConstantRearrangeable for Method {
+    fn rearrange(&mut self, rearrangements: &HashMap<u16, u16>) -> KapiResult<()> {
+        Self::rearrange_index(&mut self.name_index, rearrangements);
+        Self::rearrange_index(&mut self.descriptor_index, rearrangements);
+
+        for attribute in &mut self.attribute_infos {
+            attribute.rearrange(rearrangements)?;
+        }
+
+        Ok(())
+    }
 }
