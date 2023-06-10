0a9c3da2328bd9584f19764793a1a87366d9521c
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-06-10 17:48:51 +0800

    Fix: Fix insta tests

--- test/compile_class_file.rs
+++ test/compile_class_file.rs
@@ -2,17 +2,13 @@ use insta::assert_yaml_snapshot;
 
 use ka_pi::error::KapiResult;
 
-fn assert_class_file_parse(bytes: &[u8]) -> KapiResult<()> {
-    let class_tree = ka_pi::asm::parse::to_class(bytes)?;
+#[test]
+fn test_main() -> KapiResult<()> {
+    let class_tree = ka_pi::asm::parse::to_class(include_bytes!(
+        "../compiled_source/out/production/compiled_source/Main.class"
+    ))?;
 
     assert_yaml_snapshot!(class_tree);
 
     Ok(())
-}
-
-#[test]
-fn test_main() -> KapiResult<()> {
-    assert_class_file_parse(include_bytes!(
-        "../compiled_source/out/production/compiled_source/Main.class"
-    ))
 }
