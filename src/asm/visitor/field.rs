bfd9ba5d4b40400269cc70e9cbd5fab0c3c698ce
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-06-26 17:33:12 +0800

    Feat: Support visiting `asm::node::Field`

--- src/asm/visitor/field.rs
+++ src/asm/visitor/field.rs
@@ -1,6 +1,5 @@
 use crate::asm::node::attribute::ConstantValue;
-use crate::error::KapiResult;
 
 pub trait FieldVisitor {
-    fn visit_constant(&mut self, constant_value: ConstantValue) -> KapiResult<()>;
+    fn visit_constant(&mut self, constant_value: &mut ConstantValue);
 }
