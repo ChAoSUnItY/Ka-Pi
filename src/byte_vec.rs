8777f514ef7f0005a2be837dfcd2427f709f8e57
Kyle Lin <minecraft.kyle.train@gmail.com>
2023-09-15 01:49:24 +0800

    Inline wrapper struct ByteVec

--- src/byte_vec.rs
+++ src/byte_vec.rs
@@ -12,49 +12,36 @@ pub(crate) trait ToBytes {
     fn put_bytes(&self, vec: &mut ByteVec);
 }
 
-#[derive(Debug, Default)]
-pub(crate) struct ByteVec(Vec<u8>);
+pub trait ByteVector {
+    fn push_u8(&mut self, u8: u8) -> &mut Self;
 
-impl ByteVec {
-    pub fn with_capacity(capacity: usize) -> Self {
-        Self(Vec::with_capacity(capacity))
-    }
+    fn push_u8s(&mut self, u8s: &[u8]) -> &mut Self;
 
-    pub fn as_vec(self) -> Vec<u8> {
-        self.0
-    }
+    fn push_u16(&mut self, u16: u16) -> &mut Self;
 
-    pub fn push_u8(&mut self, byte: u8) -> &mut Self {
-        self.push(byte);
-        self
-    }
-
-    pub fn push_u8s(&mut self, bytes: &[u8]) -> &mut Self {
-        self.extend_from_slice(bytes);
-        self
-    }
-
-    pub fn push_u16(&mut self, val: u16) -> &mut Self {
-        self.push_u8s(&val.to_be_bytes());
-        self
-    }
-
-    pub fn push_u32(&mut self, val: u32) -> &mut Self {
-        self.push_u8s(&val.to_be_bytes());
-        self
-    }
+    fn push_u32(&mut self, u32: u32) -> &mut Self;
 }
 
-impl Deref for ByteVec {
-    type Target = Vec<u8>;
+pub type ByteVec = Vec<u8>;
 
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl ByteVector for ByteVec {
+    fn push_u8(&mut self, u8: u8) -> &mut Self {
+        self.push(u8);
+        self
     }
-}
 
-impl DerefMut for ByteVec {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
+    fn push_u8s(&mut self, u8s: &[u8]) -> &mut Self {
+        self.extend_from_slice(u8s);
+        self
+    }
+
+    fn push_u16(&mut self, u16: u16) -> &mut Self {
+        self.push_u8s(&u16.to_be_bytes());
+        self
+    }
+
+    fn push_u32(&mut self, u32: u32) -> &mut Self {
+        self.push_u8s(&u32.to_be_bytes());
+        self
     }
 }
